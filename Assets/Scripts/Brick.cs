aec6a6707fa3f422cabebd599ca6bcace36bb736
Joan Lorenzo Matuto <matuto1820382@mkt.ceu.edu.ph>
2022-01-19 22:37:05 +0800

    Modify project --  beat ball 2

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -6,16 +6,24 @@ public class Brick : MonoBehaviour
 {
     public int hits = 1;
     public int points = 10;
+    public Vector3 rotator;
+    public Material hitMaterial;
+
+    Material _orgMaterial;
+    Renderer _renderer;
 
     void Start()
     {
-        
+        //transform.Rotate(rotator * (transform.position.x + transform.position.y)* 0.1f);
+        _renderer = GetComponent<Renderer>();
+        _orgMaterial = _renderer.sharedMaterial;
+
     }
 
     
     void Update()
     {
-        
+        //transform.Rotate(rotator * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -25,5 +33,13 @@ public class Brick : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        _renderer.sharedMaterial = hitMaterial;
+    }
+
+    void RestoreMaterial()
+    {
+        _renderer.sharedMaterial = _orgMaterial;
+        Invoke("RestoreMaterial", 0.05f);
     }
 }
